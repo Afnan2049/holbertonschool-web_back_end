68d02467ab44ff4006266f6ebd541bcad0aa396d
Afnan2049 <alfaidi.afnan@gmail.com>
2026-05-15 19:04:44 +0300

    creating directory and files+create a function named getListStudents that returns an array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/**
+ * Returns an array of student objects.
+ * Each object contains id, firstName, and location.
+ *
+ * @returns {Array} an array of student objects.
+ */
+export default function getListStudent() {
+	return [
+		{ id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
+		{ id: 2, firstName: 'james', location: 'Columbia' },
+		{ id: 5, firstName: 'Serena', location: 'San Fransisco' },
+	];
+}
