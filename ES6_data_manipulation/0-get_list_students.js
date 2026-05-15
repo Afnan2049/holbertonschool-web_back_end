cb437dbfde8221332fcd0520b6225a7be08a2d7c
Afnan2049 <alfaidi.afnan@gmail.com>
2026-05-15 20:56:11 +0300

    fix file 0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,12 +2,12 @@
  * Returns an array of student objects.
  * Each object contains id, firstName, and location.
  *
- * @returns {Array} an array of student objects.
+ * @returns {Array} An array of student objects.
  */
-export default function getListStudent() {
-	return [
-		{ id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
-		{ id: 2, firstName: 'james', location: 'Columbia' },
-		{ id: 5, firstName: 'Serena', location: 'San Fransisco' },
-	];
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
