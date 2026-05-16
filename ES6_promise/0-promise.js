1f0490fbf2cec5443b925b4f75da6895795eae61
Afnan2049 <alfaidi.afnan@gmail.com>
2026-05-16 20:49:33 +0000

    task 0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+/**
+ * Returns a Promise.
+ * @returns {Promise} A basic JavaScript Promise.
+ */
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // For this basic task, we just need to return an instance of a Promise.
+    resolve();
+  });
+}
